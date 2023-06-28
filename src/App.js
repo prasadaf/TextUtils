307f585d75b96f313e6ffe99100f8bdafdaf3e6b
Rajneesh Prasad <prasadaf.rj@gmail.com>
2023-06-28 14:04:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+// import About from './components/About';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
+      document.querySelector(".toggleBtn").style.backgroundColor = "#a3cfbb";
+      document.querySelector(".toggleBtn").style.borderColor = "white";
+      // document.title="TextUtils - Dark mode";
+      // setInterval(()=>{
+      //   document.title="TextUtils is Amazing";
+      // },1000);
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.querySelector(".toggleBtn").style.backgroundColor = "white";
+      document.querySelector(".toggleBtn").style.borderColor = "#198754";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <BrowserRouter>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}/>
+          </Routes> 
+      </div>
+      </BrowserRouter> */}
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+let Status = false;
+export default function Textform(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was Clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to uppercase!","success");
+    }
+    const handleLoClick = () => {
+        // console.log("Uppercase was Clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase!","success");
+    }
+    const handleClearClick = () => {
+        // console.log("Uppercase was Clicked");
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text is cleared!","success");
+    }
+    const handleReverseClick = () => {
+        // console.log("Uppercase was Clicked");
+        let newText = text.split(" ").reverse().join(" ");
+        setText(newText);
+        props.showAlert("Word is Reversed!","success");
+    }
+    const handleReverseTextClick = () => {
+        // console.log("Uppercase was Clicked");
+        let newText = text.split("").reverse().join("");
+        setText(newText);
+        props.showAlert("Text is Reversed!","success");
+    }
+    const handleCopyClick = () => {
+        let Copy = document.getElementById("myBox");
+        Copy.select();
+        navigator.clipboard.writeText(text);
+        props.showAlert("Text is copied!","success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/).join(" ");
+        setText(newText);
+        props.showAlert("Extra space is Removed from text!","success");
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        if (Status === false) {
+            let x = event.target.value;
+            let y = x[x.length - 1];
+            event.target.value = y;
+            setText(event.target.value);
+            Status = true;
+        } else {
+            setText(event.target.value);
+        }
+
+    }
+    const [text, setText] = useState("Enter text here");
+    // setText("new Text");
+    return (
+        <>
+            <div className='container' style={{ color:props.mode === 'light' ? 'black' : 'white' }}>
+                <div className="mb-3">
+                    <label htmlFor="myBox" className="form-label"><h2>{props.heading}</h2></label>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'black', color: props.mode === 'light' ? 'black' : 'white' }}></textarea>
+                </div>
+                <button className="btn btn-success" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-success mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-success" onClick={handleClearClick}>Clear text</button>
+                <button className="btn btn-success mx-3" onClick={handleReverseClick}>Reverse words</button>
+                <button className="btn btn-success" onClick={handleCopyClick}>Copy text</button>
+                <button className="btn btn-success mx-3" onClick={handleExtraSpaces}>Remove extra spaces</button>
+                <button className="btn btn-success mx" onClick={handleReverseTextClick}>Reverse text</button>
+            </div>
+            <div className="container my-3" style={{ color:props.mode === 'light' ? 'black' : 'white' }}>
+                <h3>Your Text Summary</h3>
+                <p><b>{text==="Enter text here"||text===""?"0":text.split(/[ ]+/).length}</b> words and <b>{text==="Enter text here"?"0":text.split(/[ ]+/).join("").length}</b> characters</p>
+                <p><b>{text==="Enter text here"||text===""?"0.000":(text.split(/[ ]+/).length) * 0.008}</b> Minutes to read</p>
+                <h4>Preview</h4>
+                <p>{text==="Enter text here"||text===""?"Enter Some text to preview":text}</p>
+            </div>
+        </>
+    )
+}
+
+Textform.prototype = {
+    heading: PropTypes.string.isRequired,
+}
+
+Textform.defaultProps = {
+    heading: 'Enter the text to analyze',
+}
